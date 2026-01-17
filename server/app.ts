8688efe97d80b2cffa508d80b420ad23bf75d72f
Abu-Hurairah Chohan <abuchohan@hotmail.co.uk>
2026-01-17 16:53:42 +0000

    feat: add rate limiting

--- server/app.ts
+++ server/app.ts
@@ -2,8 +2,16 @@ import express from "express";
 import router from "./routes/index.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import rateLimit from "express-rate-limit";
 
 export const app = express();
+
+const apiLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  limit: 200,
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
+});
 
 app.use(
   cors({
@@ -12,6 +20,7 @@ app.use(
   })
 );
 
+app.use("/api", apiLimiter);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", router);
